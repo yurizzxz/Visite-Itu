1d89af15710cab701883399bc45e64215186a59d
yurizzxz <yuri.alves.silva@gmail.com>
2025-06-26 16:35:43 -0300

    fix: trocando stack para slot me _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,20 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-
 import TopBar from '@/components/top-bar';
-import "../global.css";
+import { useFonts } from 'expo-font';
+import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import '../global.css';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  if (!loaded) {
-    return null;
-  }
+  if (!loaded) return null;
 
   return (
     <>
       <TopBar />
-      <Stack screenOptions={{ headerShown: false }} />
+      <Slot /> 
       <StatusBar style="auto" />
     </>
   );
--- components/top-bar.tsx
+++ components/top-bar.tsx
@@ -1,18 +1,29 @@
-import Entypo from "@expo/vector-icons/Entypo";
+  import Entypo from "@expo/vector-icons/Entypo";
+import { useRouter } from "expo-router";
 import { Pressable, Text, View } from "react-native";
 
-export default function TopBar() {
-  return (
-    <View className="flex py-4 px-1.5 border-b border-[#D9D9D9] flex-row  justify-between">
-      <Pressable >
-        <Entypo name="heart-outlined" size={20} color="black" />
-      </Pressable>
-      <View>
-        <Text>LogoTeste</Text>
+  export default function TopBar() {
+    const router = useRouter()
+
+    const goToHome = () => {
+      router.push('/')
+    }
+
+    const goToFavorites = () => {
+      router.push('./extra/favorite-places')
+    }
+
+    return (
+      <View className="flex py-4 px-1.5 border-b border-[#D9D9D9] flex-row  justify-between">
+        <Pressable >
+          <Entypo name="heart-outlined" size={20} color="black" />
+        </Pressable>
+        <Pressable onPress={goToHome}>
+          <Text>LogoTeste</Text>
+        </Pressable>
+        <Pressable onPress={goToFavorites}>
+          <Entypo name="heart-outlined" size={20} color="black" />
+        </Pressable>
       </View>
-      <Pressable >
-        <Entypo name="heart-outlined" size={20} color="black" />
-      </Pressable>
-    </View>
-  );
-}
+    );
+  }
