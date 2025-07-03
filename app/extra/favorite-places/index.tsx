f79b3c7a4459ee175ad31d832de20ea253222db7
yurizzxz <yuri.alves.silva@gmail.com>
2025-07-03 19:26:01 -0300

    Atualiza página de favoritos com carregamento dinâmico e toggle de favoritos

--- app/extra/favorite-places/index.tsx
+++ app/extra/favorite-places/index.tsx
@@ -2,9 +2,10 @@ import Container from "@/components/container";
 import PlaceCard from "@/components/place-card";
 import { Place } from "@/types/place";
 import { getFavorites } from "@/utils/storage";
+import Entypo from "@expo/vector-icons/Entypo";
 import { useFocusEffect, useRouter } from "expo-router";
 import React, { useState } from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
 
 export default function FavoritosScreen() {
   const [favorites, setFavorites] = useState<Place[]>([]);
@@ -17,21 +18,31 @@ export default function FavoritosScreen() {
   );
 
   return (
-    <Container>
-      <Text className="text-2xl font-bold mb-4">Meus Favoritos</Text>
-      {favorites.length === 0 ? (
-        <Text>Nenhum local salvo ainda.</Text>
-      ) : (
-        favorites.map((place) => (
-          <TouchableOpacity
-            key={place.id}
-            onPress={() => router.push(`/extra/${place.id}`)}
-            className="flex flex-col gap-3"
-          >
-            <PlaceCard place={place} />
-          </TouchableOpacity>
-        ))
-      )}
+    <Container className="px-0">
+      <View className="border-b border-[#E1E1E1] mb-4 pt-4">
+        <View className="flex-row  items-center mb-3 gap-3 px-3 ">
+          <Pressable onPress={() => router.push("/")}>
+            <Entypo name="chevron-left" size={28} color="black" />
+          </Pressable>
+          <Text className="text-2xl font-bold ">Meus Favoritos</Text>
+        </View>
+      </View>
+
+      <View className="px-4">
+        {favorites.length === 0 ? (
+          <Text>Nenhum local salvo ainda.</Text>
+        ) : (
+          favorites.map((place) => (
+            <TouchableOpacity
+              key={place.id}
+              onPress={() => router.push(`/extra/${place.id}`)}
+              className="flex flex-col gap-3"
+            >
+              <PlaceCard place={place} />
+            </TouchableOpacity>
+          ))
+        )}
+      </View>
     </Container>
   );
 }
--- components/container.tsx
+++ components/container.tsx
@@ -4,13 +4,14 @@ import { ScrollView } from "react-native";
 
 interface ContainerProps {
   children: ReactNode;
+  className?: string;
 }
 
-export default function ({ children }: ContainerProps) {
+export default function ({ children, className }: ContainerProps) {
   const statusBarHeight = Constants.statusBarHeight;
   return (
     <ScrollView
-      className="flex-1 mt-3 px-4 bg-white"
+      className={`flex-1 bg-[#F4F3F3]px-4 ${className}`}
       style={{ paddingTop: statusBarHeight }}
     >
       {children}
